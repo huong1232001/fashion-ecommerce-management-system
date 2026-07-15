c7aac8f76d421ed0232546cc00a80f219f919511
huong1232001 <19110084@student.hcmute.edu.vn>
2026-07-16 03:08:12 +0700

    Fix ALLOWED_HOSTS for Render

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5rd$l68c=qv)6w($cw&brkcbs#=73pq+c@=@520ka_q@si8#3x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 
