4a5f753ee794660c9ca977f0bfdf03f904e0086e
huong1232001 <19110084@student.hcmute.edu.vn>
2026-07-16 03:16:23 +0700

    Fix allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-5rd$l68c=qv)6w($cw&brkcbs#=73pq+c@=@520ka_q@si8#3x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'fashion-ecommerce-management-system.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 
